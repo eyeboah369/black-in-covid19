e7869c57b9282e68a68f07e157c8777bf93e434b
Evans Yeboah Jr <eyeboahcw@gmail.com>
2020-06-25 20:57:02 -0400

    Updated color scheme

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Timeline.css'
+import Fade from 'react-reveal/Fade';
 
 function Timeline(){
     return(
@@ -7,26 +8,25 @@ function Timeline(){
 	<div class="container-fluid" style={{ width: "auto", paddingBottom: "40vh", color: "white"}}>
 		<div class="row">
 			<nav class="col-3 col-sm-2" id="verticalScrollspy">
-				<ul class="nav flex-column nav-pills">
-					<li class="nav-item"><a class="nav-link active" href="#dogs">Dogs</a></li>
-					<li class="nav-item"><a class="nav-link" href="#cats">Cats</a></li>
-					<li class="nav-item"><a class="nav-link" href="#fish">Fish</a></li>
-					<li class="nav-item"><a class="nav-link" href="#penguins">Penguins</a></li>
-					<li class="nav-item"><a class="nav-link" href="#alpacas">Alpacas</a></li>
-					<li class="nav-item"><a class="nav-link" href="#rabbits">Rabbits</a></li>
+				<ul class="nav flex-column nav-pills" >
+					<li class="nav-item" ><a class="nav-link active" href="#slavery">Slavery</a></li>
+					<li class="nav-item"><a class="nav-link" href="#cats">Emancipation</a></li>
+					<li class="nav-item"><a class="nav-link" href="#fish">Jim Crow</a></li>
+					<li class="nav-item"><a class="nav-link" href="#penguins">Civil Rights</a></li>
+					<li class="nav-item"><a class="nav-link" href="#alpacas">New Jim Crow</a></li>
+					<li class="nav-item"><a class="nav-link" href="#rabbits">Present</a></li>
 				</ul>
 			</nav>
 			<div class="col-9 col-sm-10">
-				<div id="dogs" class="container-fluid" style={{marginTop: "50px", boxShadow: "-8px 15px 10px -3px #888888", height: "95vh", backgroundColor: "#0E8181"}}>
-					<h1>Dogs</h1>
-					<p>Look at the navigation menu to see how it interacts with scrolling over this segment</p>
-					<p>Look at the navigation menu to see how it interacts with scrolling over this segment</p>
-					<p>Look at the navigation menu to see how it interacts with scrolling over this segment</p>
-					<p>Look at the navigation menu to see how it interacts with scrolling over this segment</p>
-					<p>Look at the navigation menu to see how it interacts with scrolling over this segment</p>
-					<p>Look at the navigation menu to see how it interacts with scrolling over this segment</p>
+                
+				<div id="slavery" class="container-fluid" style={{justifyContent: "center",alignItems: "center", marginTop: "50px", boxShadow: "-8px 15px 10px -3px #888888", height: "95vh", backgroundColor: "#282c34"}}>
+					<h1>Slavery</h1>
+					<strong><p>The original sin of the United States </p></strong>
+
+
 				</div>
-				<div id="cats" class="container-fluid" style={{marginTop: "50px", boxShadow: "-6px 9.5px 20px -7px #888888"}}>
+                
+				<div id="cats" class="container-fluid" style={{marginTop: "50px", boxShadow: "-8px 15px 10px -3px #888888", height: "95vh", backgroundColor: "#282c34"}}>
 					<h1>Cats</h1>
 					<p>Look at the navigation menu to see how it interacts with scrolling over this segment</p>
 					<p>Look at the navigation menu to see how it interacts with scrolling over this segment</p>
@@ -36,7 +36,7 @@ function Timeline(){
 					<p>Look at the navigation menu to see how it interacts with scrolling over this segment</p>
 					<p>Look at the navigation menu to see how it interacts with scrolling over this segment</p>
 				</div>
-				<div id="fish" class="container-fluid" style={{marginTop: "50px", boxShadow: "-6px 9.5px 20px -7px #888888"}}>
+				<div id="fish" class="container-fluid" style={{marginTop: "50px", boxShadow: "-8px 15px 10px -3px #888888", height: "95vh", backgroundColor: "#282c34"}}>
 					<h1>Fish</h1>
 					<p>Look at the navigation menu to see how it interacts with scrolling over this segment</p>
 					<p>Look at the navigation menu to see how it interacts with scrolling over this segment</p>
@@ -47,18 +47,18 @@ function Timeline(){
 					<p>Look at the navigation menu to see how it interacts with scrolling over this segment</p>
 					<p>Look at the navigation menu to see how it interacts with scrolling over this segment</p>
 				</div>
-				<div id="penguins" class="container-fluid" style={{marginTop: "50px", boxShadow: "-6px 9.5px 20px -7px #888888"}}>
+				<div id="penguins" class="container-fluid" style={{marginTop: "50px", boxShadow: "-8px 15px 10px -3px #888888", height: "95vh", backgroundColor: "#282c34"}}>
 					<h1>Penguins</h1>
 					<p>Look at the navigation menu to see how it interacts with scrolling over this segment</p>
 					<p>Look at the navigation menu to see how it interacts with scrolling over this segment</p>
 				</div>
-				<div id="alpacas" class="container-fluid" style={{marginTop: "50px", boxShadow: "-6px 9.5px 20px -7px #888888"}}>
+				<div id="alpacas" class="container-fluid" style={{marginTop: "50px", boxShadow: "-8px 15px 10px -3px #888888", height: "95vh", backgroundColor: "#282c34"}}>
 					<h1>Alpacas</h1>
 					<p>Look at the navigation menu to see how it interacts with scrolling over this segment</p>
 					<p>Look at the navigation menu to see how it interacts with scrolling over this segment</p>
 					<p>Look at the navigation menu to see how it interacts with scrolling over this segment</p>
 				</div>
-				<div id="rabbits" class="container-fluid" style={{marginTop: "50px", boxShadow: "-6px 9.5px 20px -7px #888888"}}>
+				<div id="rabbits" class="container-fluid" style={{marginTop: "50px", boxShadow: "-6px 9.5px 20px -7px #888888", height: "95vh", backgroundColor: "#282c34"}}>
 					<h1>Rabbits</h1>
 					<p>Look at the navigation menu to see how it interacts with scrolling over this segment</p>
 					<p>Look at the navigation menu to see how it interacts with scrolling over this segment</p>
